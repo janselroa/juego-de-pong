71c1fb82006ebaec54ae87fc8c6b6b54df4793f0
jansel <janselroa2424@gmail.com>
2021-04-17 17:57:10 -0400

    mejore el juego

--- main.py
+++ main.py
@@ -2,80 +2,15 @@ import pygame
 import sys
 from objects import *
 
-pygame.init()
+def main():
+    pygame.init()
+    windows=pygame.display.set_mode((800, 600))
 
-# CONSTANTES
-FONDO=pygame.image.load('img/background.png')
-CLOCK=pygame.time.Clock()
-WIDTH, HEIGHT=800, 600
-SIDE=(WIDTH, HEIGHT)
-FONT=pygame.font.Font(None, 35)
-# colores
-BLACK=(0,0,0)
-WHITE=(244,244, 244)
-
-windows=pygame.display.set_mode(SIDE)
-pygame.display.set_caption('Pong')
-
-sprites_list=pygame.sprite.Group()
-
-# players
-player1=Player()
-player1.rect.y=300
-player1.rect.x=20
-sprites_list.add(player1)
-
-player2=Player()
-player2.rect.y=300
-player2.rect.x=750
-sprites_list.add(player2)
-
-# pelota
-pelota=Pelota()
-pelota.rect.x=400
-pelota.rect.y=300
-sprites_list.add(pelota)
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-
-        if event.type == pygame.KEYDOWN:
-        	if event.key == pygame.K_w:
-        		player1.changespeed(-3)
-        	if event.key == pygame.K_s:
-        		player1.changespeed(3)
-        	if event.key == pygame.K_UP:
-        		player2.changespeed(-3)
-        	if event.key == pygame.K_DOWN:
-        		player2.changespeed(3)
-        
-        if event.type == pygame.KEYUP:
-        	if event.key == pygame.K_w:
-        		player1.changespeed(0)
-        	if event.key == pygame.K_s:
-        		player1.changespeed(0)
-       		
-       		if event.key == pygame.K_UP:
-        		player2.changespeed(0)
-        	if event.key == pygame.K_DOWN:
-        		player2.changespeed(0)
-
-    # detectando colisiones
-    if pygame.sprite.collide_rect(pelota, player1) or  pygame.sprite.collide_rect(pelota, player2):
-    	pelota.velocidad_x*=-1
-    	pelota.velocidad_y*=-1
-    if pelota.rect.x <3:
-    	player2.score+=1
-    if pelota.rect.x >798:
-    	player1.score+=1
-    windows.blit(FONDO, (0, 0))
-    text_puntaje_p1=FONT.render(f'{player1.score}', 35,WHITE)
-    text_puntaje_p2=FONT.render(f'{player2.score}', 35,WHITE)
-    windows.blit(text_puntaje_p1,(30, 20))
-    windows.blit(text_puntaje_p2,(700, 20))
-    sprites_list.draw(windows)
-    sprites_list.update()
-    pygame.display.flip()
-    CLOCK.tick(120)
+    game=Game()
+    while True:
+        game.process_event()
+        game.run_logic()
+        game.display_frame(windows)
+        game.CLOCK.tick(100)
+if __name__ == '__main__':
+    main()
--- objects.py
+++ objects.py
@@ -1,16 +1,22 @@
-import pygame
+import pygame, sys
+import random
 
 pygame.init()
+
+#colores rgb
 BLACK=(0, 0, 0)
+WHITE=(255, 255, 255)
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y):
         super().__init__()
         self.image=pygame.image.load('img/player.png')
         self.image.set_colorkey(BLACK)
         self.rect=self.image.get_rect()
         self.velocidad=0
         self.score=0
+        self.rect.x=x
+        self.rect.y=y
 
     def changespeed(self,v):
         self.velocidad=v
@@ -26,8 +32,8 @@ class Pelota(pygame.sprite.Sprite):
         self.image=pygame.image.load('img/pelota.png')
         self.image.set_colorkey(BLACK)
         self.rect=self.image.get_rect()
-        self.velocidad_x=3
-        self.velocidad_y=3
+        self.velocidad_x=4
+        self.velocidad_y=5
 
 
     def update(self):
@@ -40,4 +46,75 @@ class Pelota(pygame.sprite.Sprite):
         if self.rect.x>800 or self.rect.x<0:
             self.rect.x=400
             self.rect.y=300
-            self.velocidad_y*=-1            
+            self.velocidad_y*=-1
+
+class Game:
+    def __init__(self):
+        self.FONDO=pygame.image.load('img/background.png')
+        self.CLOCK=pygame.time.Clock()
+        self.WIDTH=800 
+        self.HEIGHT=600
+        self.SIDE=(self.WIDTH, self.HEIGHT)
+        self.FONT=pygame.font.Font(None, 35)
+        self.BLACK=(0,0,0)
+        self.WHITE=(244,244, 244)
+
+        #sprites
+        self.all_sprites_list=pygame.sprite.Group()
+        self.players_list=pygame.sprite.Group()
+
+        self.player1=Player(20, 300)
+        self.player2=Player(750,300)
+        
+        self.pelota=Pelota()
+
+        self.all_sprites_list.add(self.player1)
+        self.all_sprites_list.add(self.player2)
+        self.all_sprites_list.add(self.pelota)
+
+    def process_event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    self.player1.changespeed(-3)
+                if event.key == pygame.K_s:
+                    self.player1.changespeed(3)
+                if event.key == pygame.K_UP:
+                    self.player2.changespeed(-3)
+                if event.key == pygame.K_DOWN:
+                    self.player2.changespeed(3)
+            
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    self.player1.changespeed(0)
+                if event.key == pygame.K_s:
+                    self.player1.changespeed(0)
+                
+                if event.key == pygame.K_UP:
+                    self.player2.changespeed(0)
+                if event.key == pygame.K_DOWN:
+                    self.player2.changespeed(0)
+
+    def run_logic(self):
+        # detectando colisiones
+        if pygame.sprite.collide_rect(self.pelota, self.player1) or  pygame.sprite.collide_rect(self.pelota, self.player2):
+            self.pelota.velocidad_x*=-1
+            self.pelota.velocidad_y*=-1
+            self.pelota.velocidad_y=random.randint(1, 10)
+
+        if self.pelota.rect.x <3:
+            self.player2.score+=1
+        if self.pelota.rect.x >798:
+            self.player1.score+=1
+
+    def display_frame(self, windows):
+        windows.blit(self.FONDO, (0, 0))
+        self.all_sprites_list.draw(windows)
+        self.all_sprites_list.update()
+        self.text_puntaje_p1=self.FONT.render(f'{self.player1.score}', 35,WHITE)
+        self.text_puntaje_p2=self.FONT.render(f'{self.player2.score}', 35,WHITE)
+        windows.blit(self.text_puntaje_p1,(30, 20))
+        windows.blit(self.text_puntaje_p2,(700, 20))
+        pygame.display.flip()
